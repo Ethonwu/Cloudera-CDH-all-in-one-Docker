a6be92b5959d11812f637692c11f77b06f6e6354
ethonwu <a5351998@gmail.com>
2020-02-05 16:34:07 +0800

    Mark sth

--- script/run.sh
+++ script/run.sh
@@ -9,10 +9,11 @@ echo never > /sys/kernel/mm/transparent_hugepage/defrag
 /etc/init.d/mysql start
 /etc/init.d/cloudera-scm-agent start
 /etc/init.d/cloudera-scm-server start 
-#tailf /var/log/cloudera-scm-server/cloudera-scm-server.log
-while [ `curl -s -X GET -u "admin:admin"  http://localhost:7180/api/version` -z ] ;
-    do
-    echo "waiting 10s for CM to come up..";
-    sleep 10;
-done
-python2.7 create_cluster.py ./cm-deployment.json 
+tailf /var/log/cloudera-scm-server/cloudera-scm-server.log
+#while [ `curl -s -X GET -u "admin:admin"  http://localhost:7180/api/version` -z ] ;
+#    do
+#    echo "waiting 10s for CM to come up..";
+#    sleep 10;
+#done
+#python2.7 create_cluster.py ./cm-deployment.json 
+#sh ./CM_service_change.sh stop 
