2a443673bd801a3232397451aa34a2a2534ef584
ethonwu <a5351998@gmail.com>
2020-02-15 11:17:24 +0800

    Add on/off cm command

---
+++ script/CM_service_change.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -X POST -u "admin:admin" http://127.0.0.1:7180/api/v19/cm/service/commands/$1
+curl -X POST -u "admin:admin" http://127.0.0.1:7180/api/v19/clusters/Cluster%201/commands/$1
+
